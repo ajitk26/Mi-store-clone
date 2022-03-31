fef104cd2675007f215a4ec88dacf05cdd2b8425
Ajit kumar <aky8062@gmail.com>
2022-03-31 12:49:52 +0530

    getting ready

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import PreNavbar from './components/PreNavbar.js';
+import Navbar from './components/Navbar.js';
+import {BrowserRouter as Router} from 'react-router-dom'
+import Slider from './components/Slider.js'
+import data from './data/data.json'
+import Offers from './components/Offers.js'
+
+const {banner}=data;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <PreNavbar/>
+      <Navbar/>
+      <Slider start={banner.start}/>
+      <Offers/>
+    </Router>
   );
 }
-
 export default App;
